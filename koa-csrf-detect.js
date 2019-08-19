0353d176162f44fa3db6a6c15fe14354649de596
jiajianrong <>
2019-08-19 15:24:54 +0800

    first

--- koa-csrf-detect.js
+++ koa-csrf-detect.js
@@ -1,9 +1,11 @@
 // csrf防御规则
-// step1，判别是否post，否的话无需防御
-// step2，判别是否当前url属于白名单：是则无需防御
-// step3, 获取cookie ppu：如果未获取，则认为无需防御
-// step4, ppu存在：获取 http header x-csrf-token
-// step5，判断x-csrf-token与ppu是否相同：是则认为通过校验，否则拒绝服务
+/*
+- step1，判别是否post，否的话无需防御
+- step2，判别是否当前url属于白名单：是则无需防御
+- step3，获取cookie ppu：如果未获取，则认为无需防御
+- step4，ppu存在：获取 http header x-csrf-token
+- step5，判断x-csrf-token与ppu是否相同：是则认为通过校验，否则拒绝服务
+*/
 
 
 /**
